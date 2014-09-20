3d7d30c958b7fa2efd217b20a9b1e4c499591a2a
Nathan Fig <figuernd@miamioh.edu>
2014-09-20 17:15:51 -0400

    fixedparameters

--- RAIDER_eval.py
+++ RAIDER_eval.py
@@ -247,7 +247,7 @@ def run_bigfoot(input_file, bigfoot_dir, L, C, I, T):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
-    cmd = "{bigfoot} -l {L} -c {C} -i {I} -t {T} {bigfoot_dir} {input_file}".format(bigfoot = Locations['bigfoot'], L = L, C = C, I = I, T = T, bigfoot_dir = bigfoot_dir, input_file = input_file)   # Put the command-line executable for for bigfoot here.  Use input_file for the input file name, and put any output into bigfoot_dir
+    cmd = "{bigfoot} -l {L} -c {C} --I {I} --T {T} {bigfoot_dir} {input_file}".format(bigfoot = Locations['bigfoot'], L = L, C = C, I = I, T = T, bigfoot_dir = bigfoot_dir, input_file = input_file)   # Put the command-line executable for for bigfoot here.  Use input_file for the input file name, and put any output into bigfoot_dir
 
     if show_progress:
         show_progress.write("Launching bigfoot:\n%s\n" % (cmd))
