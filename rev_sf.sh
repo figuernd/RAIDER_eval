02b421bb9d4b0044864bf48b55f02434af0860fe
schaefce <schaefce@miamioh.edu>
2015-08-05 15:41:08 -0400

    Added script for running RAIDER_eval on phRAIDER

---
+++ rev_sf.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+cd ~/karro/RAIDER_eval/
+##comp=comparisons_overlap3
+
+
+
+if [ -n "$sfile" ]; then
+    id=$(basename $sfile .txt)
+    seed_str="--sf ${sfile}"
+else
+    L=${#seed}
+    weight=$(grep -o "1" <<< "$seed" | wc -l)
+    id=L${L}_W${weight}_S${seed}
+    seed_str="-s ${seed}"
+fi
+
+echo $id
+basedata=${data##*/}
+fname=${dir}/F${freq}_${id}
+
+
+module load python-3.3.3
+##data=hg18.chr22.fa
+
+opts_dir_str=""
+opts=""
+#if [ -v $PS ]; then
+#    opts="${opts} --ps"
+#    opts_dir_str=${opts_dir_str}_PROSPLIT
+#fi
+
+
+echo "Output directory: ${fname}"
+echo "Data: ${basedata}"
+echo "Seed file: ${sfile}"
+
+if [ ${type} == "chrom_sim" ]; then
+    fname=${dir}/sim/F${freq}_${id}
+    if [ -z $T ]; then
+        t_str=""
+        T=1
+    else
+        t_str="-t ${T}"
+    fi
+    if [ -z $ST ]; then
+        st_str=""
+        ST=0
+    else
+        st_str="--st ${ST}"
+    fi
+    if [ -z $RNG ]; then
+        rng_str=""
+        RNG=None
+    else
+        rng_str="--rng_seed ${RNG}"
+    fi
+    fname=${fname}_T${T}_ST${ST}_RNG${RNG}
+    
+    if [ -z $FAM ]; then
+        fam_str=""
+    else
+        fam_str="--family_file ${FAM}"
+        fname=${fname}_FAM
+    fi
+
+
+    ./RAIDER_eval.py --R2 -R --nuke -r $fname -f ${freq} ${seed_str} --age 1  --timing_jobs chrom_sim ${t_str} ${st_str} ${rng_str} ${fam_str} ${data} 
+    mv ${fname}/SOURCE_DATA/ sim_data/SIM_T${T}_ST${ST}_RNG${RNG}_FAM${FAM}
+else
+    fname=${dir}/seq/F${freq}_${id}_${opts_dir_str}
+    #./RAIDER_eval.py --R2 -R --PRA --nuke -r $fname -f ${freq} ${seed_str} --age 1 --timing_jobs seq_files ${data} 
+    ./RAIDER_eval.py --R2 --RA --nuke ${opts} --no -r $fname -f ${freq} ${seed_str} --timing_jobs seq_files ${data} 
+fi
+
+dir=${fname} ./cleanup.sh
+
+./doSortedAnalysis.R -f ${fname}/stats.txt -v -a -m -c
