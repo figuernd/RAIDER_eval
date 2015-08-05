268d06e027a7683f73c8937f71a3089bf0c80b56
schaefce <schaefce@miamioh.edu>
2015-08-05 04:43:19 -0400

    licensing

--- RAIDER_src_v2_options_new/src/SeedChain.h
+++ RAIDER_src_v2_options_new/src/SeedChain.h
@@ -1,3 +1,20 @@
+// SeedChain.h is part of phRAIDER.
+//
+// phRAIDER is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// phRAIDER is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with phRAIDER.  If not, see <http://www.gnu.org/licenses/>.
+
+// Created by Carly Schaeffer, Nathan Figueroa, and John Karro
+
 #ifndef RAIDER2_SEEDCHAIN_H
 #define RAIDER2_SEEDCHAIN_H
 
